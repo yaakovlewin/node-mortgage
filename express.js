cd62f89b813d970636d830d36a271b03a544e367
yanky levine <ynslewin@gmail.com>
2021-02-23 00:58:30 +0000

    first node app, firt commit

---
+++ express.js
@@ -0,0 +1,27 @@
+let express = require('express')
+
+let path = require('path')
+
+let ourApp = express();
+
+
+
+//ourApp.use(express.static('MY WEB'))
+
+ourApp.listen(5501)
+console.log('running')
+ourApp.get('/', (req, res) => {
+    console.log(req)
+    res.sendFile(path.join(__dirname + '/my.html'))
+})
+
+ourApp.get('/my.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/my.html'))
+})
+
+ourApp.get('/about', (req, res) => {
+    res.send(`<h1>ff</h1>`)
+
+})
+
+ourApp.use('/assets', (express.static(__dirname + '/public')))
